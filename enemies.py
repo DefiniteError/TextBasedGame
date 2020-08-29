00a45233a56ffa05ae86a116d4af116bd99d4992
DefiniteError <63715176+DefiniteError@users.noreply.github.com>
2020-08-29 16:51:19 +0100

    Add files via upload

---
+++ enemies.py
@@ -0,0 +1,43 @@
+NAME = ''
+DESCRIPTION = ''
+HEALTH = 50
+ATTACKRANGE = []
+TYPE = ''
+
+ENEMIES = {
+    'a4': { # ENEMY
+        NAME = '',
+        DESCRIPTION = '',
+        HEALTH = 50,
+        ATTACKRANGE = [8, 12]
+        TYPE = 'Enemy'
+    },
+    'c3': { # MEGA BOSS
+        NAME = '',
+        DESCRIPTION = '',
+        HEALTH = 50,
+        ATTACKRANGE = [8, 12]
+        TYPE = 'Boss'
+    },
+    'd1': { # MINI BOSS
+        NAME = '',
+        DESCRIPTION = '',
+        HEALTH = 50,
+        ATTACKRANGE = [8, 12]
+        TYPE = 'Mini Boss'
+    },
+    'b1': { # ENEMY
+        NAME = '',
+        DESCRIPTION = '',
+        HEALTH = 50,
+        ATTACKRANGE = [8, 12]
+        TYPE = 'Enemy'
+    },
+    'd4': { # MINI BOSS
+        NAME = '',
+        DESCRIPTION = '',
+        HEALTH = 50,
+        ATTACKRANGE = [8, 12]
+        TYPE = 'Mini Boss'
+    },
+}
