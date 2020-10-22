9f60fb1cfb85aca2d873816683f07db464ff5f55
Faisal Mughal <63715176+Kalonic@users.noreply.github.com>
2020-10-22 20:31:41 +0100

    Update enemies.py

--- enemies.py
+++ enemies.py
@@ -6,38 +6,38 @@ TYPE = ''
 
 ENEMIES = {
     'a4': { # ENEMY
-        NAME = '',
-        DESCRIPTION = '',
-        HEALTH = 50,
-        ATTACKRANGE = [8, 12]
-        TYPE = 'Enemy'
+        NAME : '',
+        DESCRIPTION : '',
+        HEALTH : 50,
+        ATTACKRANGE : [8, 12]
+        TYPE : 'Enemy'
     },
     'c3': { # MEGA BOSS
-        NAME = '',
-        DESCRIPTION = '',
-        HEALTH = 50,
-        ATTACKRANGE = [8, 12]
-        TYPE = 'Boss'
+        NAME : '',
+        DESCRIPTION : '',
+        HEALTH : 50,
+        ATTACKRANGE : [8, 12]
+        TYPE : 'Boss'
     },
     'd1': { # MINI BOSS
-        NAME = '',
-        DESCRIPTION = '',
-        HEALTH = 50,
-        ATTACKRANGE = [8, 12]
-        TYPE = 'Mini Boss'
+        NAME : '',
+        DESCRIPTION : '',
+        HEALTH : 50,
+        ATTACKRANGE : [8, 12]
+        TYPE : 'Mini Boss'
     },
     'b1': { # ENEMY
-        NAME = '',
-        DESCRIPTION = '',
-        HEALTH = 50,
-        ATTACKRANGE = [8, 12]
-        TYPE = 'Enemy'
+        NAME : '',
+        DESCRIPTION : '',
+        HEALTH : 50,
+        ATTACKRANGE : [8, 12]
+        TYPE : 'Enemy'
     },
     'd4': { # MINI BOSS
-        NAME = '',
-        DESCRIPTION = '',
-        HEALTH = 50,
-        ATTACKRANGE = [8, 12]
-        TYPE = 'Mini Boss'
+        NAME : '',
+        DESCRIPTION : '',
+        HEALTH : 50,
+        ATTACKRANGE : [8, 12]
+        TYPE : 'Mini Boss'
     },
 }
